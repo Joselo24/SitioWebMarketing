6b1bad315ae7ad2480d4aa89bb17a77061bf9203
José Catagua <joesitop@gmail.com>
2016-06-23 04:03:27 -0500

    ultimos cambios

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html>
 	<head>
-		<title>Porcelana Fr&iacute;a</title>
+		<title>Manualidades en Porcelana Fr&iacute;a</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
